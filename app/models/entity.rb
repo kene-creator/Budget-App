7d3ec58465b5c77c580ca39025dd8d8bd0a9f7a7
Kene <kayyungx@gmail.com>
2023-02-13 11:33:23 +0100

    Add relationships and validation

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,4 @@
 class Entity < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :groups
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+    belongs_to :user, class_name: 'User'
+    has_many :entities
 end
