8f3039beb4edd9a6ece715afbbd2de93fa036383
Kene <kayyungx@gmail.com>
2023-02-14 18:46:50 +0100

    Add group entity routes and more styling

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :entities
+
+  def total_price
+  Entity.where(group_id: id).pluck(:amount).sum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :groups do
+    resources :entities
+  end
+  
   resources :entities, only: %i[index new create destroy]
   resources :groups, only: %i[index new create destroy]
   # root 'entities#index'
