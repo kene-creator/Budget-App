6b65ac243432afe1b9c57012df6834933c49018e
Kene <kayyungx@gmail.com>
2023-02-15 09:55:49 +0100

    Add more styling to transaction page

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,9 @@
 class Entity < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :groups
+
+  def total_price
+    transaction_prices = Entity.pluck(:amount)
+    transaction_prices.sum
+  end
 end
