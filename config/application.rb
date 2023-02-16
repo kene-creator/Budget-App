39ded2efdf55dd65342667c1797b5f56e3797de9
Kene <kayyungx@gmail.com>
2023-02-16 23:08:01 +0100

    update permission

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module BudgetApp
   class Application < Rails::Application
+    config.secret_key_base = 'blipblapblup'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
