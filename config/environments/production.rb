feb9888091e3d9afb58cc0c2ec1390f397912d70
Kene <kayyungx@gmail.com>
2023-02-16 22:41:42 +0100

    remove config host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "budget-app-production-421b.up.railway.app"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
