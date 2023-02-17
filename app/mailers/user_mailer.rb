aeb91d5b9a4c50e488b6f8b02c1cc209c61093cc
Kene <kayyungx@gmail.com>
2023-02-17 13:18:02 +0100

    correct lint error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
-    def confirmation_email(user)
-        @user = user
-        mail(to: @user.email, subject: 'Confirm your email address')
-      end
+  def confirmation_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Confirm your email address')
+  end
 end
